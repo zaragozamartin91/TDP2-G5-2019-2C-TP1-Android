eaa83365699d1ebeb5d017b070ee2f75f76ba71a
zaragozamartin91 <zaragozamartin91@gmail.com>
2019-09-10 22:49:44 -0300

    Closes #47 Se desarrollo la pantalla de login

---
+++ app/src/main/java/com/g5/tdp2/myhealthapp/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.g5.tdp2.myhealthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
