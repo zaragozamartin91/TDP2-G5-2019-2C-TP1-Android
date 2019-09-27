3b81652fbf831ab7be6cd9458ee6d941ed6079c7
zaragozamartin91-despegar <martin.zaragoza@despegar.com>
2019-09-27 09:36:18 -0300

    Se cambio el hub activity de un listactivity a un app compat activity

--- app/src/main/java/com/g5/tdp2/myhealthapp/ui/HubActivity.java
+++ app/src/main/java/com/g5/tdp2/myhealthapp/ui/HubActivity.java
@@ -2,9 +2,7 @@ package com.g5.tdp2.myhealthapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,44 +10,46 @@ import android.widget.Toast;
 import com.g5.tdp2.myhealthapp.R;
 import com.g5.tdp2.myhealthapp.entity.Member;
 
-import java.io.Serializable;
-
-public class HubActivity extends ListActivity {
+public class HubActivity extends AppCompatActivity {
     public static final String MEMBER_EXTRA = "member";
-    static final String[] MOBILE_OS =
-            new String[]{"Listar profesionales", "Profesionales cercanos"};
+    static final String[] VIEWS = new String[]{"Listar profesionales", "Profesionales cercanos"};
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setListAdapter(new MobileArrayAdapter(this, MOBILE_OS));
-
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-
-        //get selected items
-        String selectedValue = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
-
-    }
-
-
-//
-//    private Member member;
-//
 //    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
+//    public void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_hub);
 //
-//        member = (Member) getIntent().getSerializableExtra(MEMBER_EXTRA);
-//
-//        TextView title = findViewById(R.id.hub_title);
-//        title.setText(getString(R.string.main_hub_title, member.getFirstname()));
-//
+//        setListAdapter(new MobileArrayAdapter(this, VIEWS));
 //
 //    }
+//
+//    @Override
+//    protected void onListItemClick(ListView l, View v, int position, long id) {
+//
+//        //get selected items
+//        String selectedValue = (String) getListAdapter().getItem(position);
+//        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+//
+//    }
+
+
+    private Member member;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hub);
+
+        member = (Member) getIntent().getSerializableExtra(MEMBER_EXTRA);
+
+        TextView title = findViewById(R.id.hub_title);
+        title.setText(getString(R.string.main_hub_title, member.getFirstname()));
+
+        ListView listView = findViewById(R.id.main_hub_list_view);
+        MobileArrayAdapter listAdapter = new MobileArrayAdapter(this, VIEWS);
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedValue = listAdapter.getItem(position);
+            Toast.makeText(HubActivity.this, selectedValue, Toast.LENGTH_SHORT).show();
+        });
+    }
 }
