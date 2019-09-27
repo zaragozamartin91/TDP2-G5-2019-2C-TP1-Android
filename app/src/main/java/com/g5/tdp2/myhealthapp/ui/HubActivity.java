3a4bffa768dde20025d43bc58a4bfcbb10e17e60
zaragozamartin91-despegar <martin.zaragoza@despegar.com>
2019-09-27 09:24:05 -0300

    Se hace un prototipo del hub como un listActivity

--- app/src/main/java/com/g5/tdp2/myhealthapp/ui/HubActivity.java
+++ app/src/main/java/com/g5/tdp2/myhealthapp/ui/HubActivity.java
@@ -2,30 +2,54 @@ package com.g5.tdp2.myhealthapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.g5.tdp2.myhealthapp.R;
 import com.g5.tdp2.myhealthapp.entity.Member;
 
 import java.io.Serializable;
 
-public class HubActivity extends AppCompatActivity {
+public class HubActivity extends ListActivity {
     public static final String MEMBER_EXTRA = "member";
-
-    private Member member;
+    static final String[] MOBILE_OS =
+            new String[]{"Listar profesionales", "Profesionales cercanos"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hub);
 
-        member = (Member) getIntent().getSerializableExtra(MEMBER_EXTRA);
-
-        TextView title = findViewById(R.id.hub_title);
-        title.setText(getString(R.string.hub_title, member.getFirstname()));
-
+        setListAdapter(new MobileArrayAdapter(this, MOBILE_OS));
 
     }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        //get selected items
+        String selectedValue = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+//
+//    private Member member;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_hub);
+//
+//        member = (Member) getIntent().getSerializableExtra(MEMBER_EXTRA);
+//
+//        TextView title = findViewById(R.id.hub_title);
+//        title.setText(getString(R.string.main_hub_title, member.getFirstname()));
+//
+//
+//    }
 }
---
+++ app/src/main/java/com/g5/tdp2/myhealthapp/ui/MobileArrayAdapter.java
@@ -0,0 +1,50 @@
+package com.g5.tdp2.myhealthapp.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.g5.tdp2.myhealthapp.R;
+
+public class MobileArrayAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+
+    public MobileArrayAdapter(Context context, String[] values) {
+        super(context, R.layout.list_mobile, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.list_mobile, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
+        textView.setText(values[position]);
+
+        // Change icon based on name
+        String s = values[position];
+
+        System.out.println(s);
+
+//        if (s.equals("WindowsMobile")) {
+//            imageView.setImageResource(R.drawable.windowsmobile_logo);
+//        } else if (s.equals("iOS")) {
+//            imageView.setImageResource(R.drawable.ios_logo);
+//        } else if (s.equals("Blackberry")) {
+//            imageView.setImageResource(R.drawable.blackberry_logo);
+//        } else {
+//            imageView.setImageResource(R.drawable.android_logo);
+//        }
+
+        return rowView;
+    }
+}
