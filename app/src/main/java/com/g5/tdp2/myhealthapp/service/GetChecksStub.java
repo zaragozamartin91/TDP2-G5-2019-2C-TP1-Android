98f96bff4fabf0cc174252a357c9ae8fa454d7f1
zaragozamartin91 <zaragozamartin91@gmail.com>
2019-11-10 13:29:01 -0300

    Se agrega soporte para OBSERVACIONES en los estudios

--- app/src/main/java/com/g5/tdp2/myhealthapp/service/GetChecksStub.java
+++ app/src/main/java/com/g5/tdp2/myhealthapp/service/GetChecksStub.java
@@ -20,7 +20,8 @@ public class GetChecksStub implements GetChecks {
                         1,
                         affiliateId,
                         DateFormatter.YYYY_MM_DD.deserialize("2019-10-01"),
-                        DateFormatter.YYYY_MM_DD.deserialize("2019-10-17")),
+                        DateFormatter.YYYY_MM_DD.deserialize("2019-10-17"),
+                        ""),
                 new Check(2,
                         "https://firebasestorage.googleapis.com/v0/b/lustrous-bay-252022.appspot.com/o/myhealthapp%2Fchecks%2Fpending%2F962f87c9-3ea7-4770-bd89-b0e0817c32fe.jpg?alt=media&token=a451095b-212a-4be3-80ef-04ac9c178b1f",
                         "myhealthapp/checks/pending/962f87c9-3ea7-4770-bd89-b0e0817c32fe.jpg",
@@ -29,7 +30,8 @@ public class GetChecksStub implements GetChecks {
                         2,
                         affiliateId,
                         DateFormatter.YYYY_MM_DD.deserialize("2019-08-11"),
-                        DateFormatter.YYYY_MM_DD.deserialize("2019-08-22"))
+                        DateFormatter.YYYY_MM_DD.deserialize("2019-08-22"),
+                        "")
         );
         succCallback.accept(checks);
     }
